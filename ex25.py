ae81aff7fafa122957b8d18305919438e4908af6
soesoewai <soesoewai143@gmail.com>
2018-06-09 01:14:04 +0630

    ex25.py

--- ex25.py
+++ ex25.py
@@ -1,43 +1,35 @@
-print("Let's practice everything.")
-print('You\'d need to know \'bout escapes with \\ that do:')
-print('\n newlines and \t tabs.')
+def break_words(stuff):
+    """This function will break up words for us."""
+    words = stuff.split(' ')
+    return words
 
-poem = """
-\tThe lovely world
-with logic so firmly planted
-cannot discern \n the needs of love
-nor comprehend passion from intuition
-and requires an explanation
-\n\t\twhere there is none.
-"""
+def sort_words(words):
+    """Sorts the words."""
+    return sorted(words)
 
-print("--------------")
-print(poem)
-print("--------------")
+def print_first_word(words):
+    """Prints the first word after popping it off."""
+    word = words.pop(0)
+    print(word)
 
+def print_last_word(words):   
+    """Prints the last word after popping it off."""
+    word = words.pop(-1)
+    print(word)
 
-five = 10 - 2 + 3 - 6
-print(f"This should be five: {five}")
+def sort_sentence(sentence):
+    """Takes in a full sentence and returns the sorted words."""
+    words = break_words(sentence)
+    return sort_words(words)
 
-def secret_formula(started):
-    jelly_beans = started * 500
-    jars = jelly_beans / 1000
-    crates = jars / 100
-    return jelly_beans, jars, crates
+def print_first_and_last(sentence):
+    """Prints the first and last words of the sentence."""
+    words = break_words(sentence)
+    print_first_word(words)
+    print_last_word(words)
 
-
-start_point = 10000
-beans, jars, crates = secret_formula(start_point)
-
-# remember that this is another way to format a string
-print("With a starting point of: {}".format(start_point))
-# it's just like with an f"" string
-print(f"We'd have {beans} beans, {jars} jars, and {crates} crates.")
-
-start_point = start_point / 10
-
-print("We can also do that this way:")
-formula = secret_formula(start_point)
-# this is an easy way to apply a list to a format string
-print("We'd have {} beans, {} jars, and {} crates.".format(*formula))
-
+def print_first_and_last_sorted(sentence):
+    """Sorts the words then prints the first and last one."""
+    words = sort_sentence(sentence)
+    print_first_word(words)
+    print_last_word(words)
