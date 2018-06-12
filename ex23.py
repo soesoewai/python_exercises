6fcf71b25a7955c10f4c7ebabac6b7fb82f35255
soesoewai <soesoewai143@gmail.com>
2018-06-12 13:45:28 +0630

    add ex23.py

--- ex23.py
+++ ex23.py
@@ -6,8 +6,8 @@ def main(language_file, encoding, errors):
     line = language_file.readline()
 
     if line:
-       print_line(line, encoding, errors)
-       return main(language_file, encoding, errors)
+        print_line(line, encoding, errors)
+        return main(language_file, encoding, errors)
 
 
 def print_line(line, encoding, errors):
