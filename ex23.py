7974ff19f5eca36edc423098a1b451e7452478af
soesoewai <soesoewai143@gmail.com>
2018-06-13 21:43:37 +0630

    add ex23.py

--- ex23.py
+++ ex23.py
@@ -1,6 +1,5 @@
 import sys
-script, input_encoding, error = sys.argv
-
+script, encoding, error = sys.argv
 
 def main(language_file, encoding, errors):
     line = language_file.readline()
@@ -9,7 +8,6 @@ def main(language_file, encoding, errors):
         print_line(line, encoding, errors)
         return main(language_file, encoding, errors)
 
-
 def print_line(line, encoding, errors):
     next_lang = line.strip()
     raw_bytes = next_lang.encode(encoding, errors=errors)
@@ -17,7 +15,6 @@ def print_line(line, encoding, errors):
 
     print(raw_bytes, "<===>", cooked_string)
 
-
 languages = open("languages.txt", encoding="utf-8")
 
-main(languages, input_encoding, error)
+main(languages, encoding, error)
